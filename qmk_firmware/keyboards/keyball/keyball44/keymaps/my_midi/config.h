1f4e6595da226d0f81fed892c893ea5379d36d61
KatsuraLab <takumikun0718@gmail.com>
2026-03-09 22:07:28 +0900

    fix(keymaps): disable rgblight to fit midi firmware size

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_midi/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_midi/config.h
@@ -22,19 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
 
-#ifdef RGBLIGHT_ENABLE
-//#    define RGBLIGHT_EFFECT_BREATHING
-//#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//#    define RGBLIGHT_EFFECT_SNAKE
-//#    define RGBLIGHT_EFFECT_KNIGHT
-//#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//#    define RGBLIGHT_EFFECT_RGB_TEST
-//#    define RGBLIGHT_EFFECT_ALTERNATING
-//#    define RGBLIGHT_EFFECT_TWINKLE
-#endif
-
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
