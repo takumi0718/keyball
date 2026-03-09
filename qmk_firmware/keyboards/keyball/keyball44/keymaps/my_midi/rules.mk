2d13f9fa4eecef631d72cdb4099768c06f91cf74
KatsuraLab <takumikun0718@gmail.com>
2026-03-09 21:05:59 +0900

    feat(keymaps): create keymap for midi

---
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_midi/rules.mk
@@ -0,0 +1,11 @@
+RGBLIGHT_ENABLE = yes
+
+OLED_ENABLE = yes
+
+VIA_ENABLE = yes
+
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+
+MIDI_ENABLE = yes           # MIDI controls
+
+AUDIO_ENABLE = no           # Audio output on port C6
